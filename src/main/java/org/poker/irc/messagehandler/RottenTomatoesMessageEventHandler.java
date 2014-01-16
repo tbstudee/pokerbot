043b9143b5767acebe19843dd4cf05b9eeaf7161
willhunting <bh3zd@mail.missouri.edu>
2014-01-16 15:58:22 -0600

    clean up some stuff

--- src/main/java/org/poker/irc/messagehandler/RottenTomatoesMessageEventHandler.java
+++ src/main/java/org/poker/irc/messagehandler/RottenTomatoesMessageEventHandler.java
@@ -20,25 +20,29 @@ public class RottenTomatoesMessageEventHandler implements MessageEventHandler {
   public void onMessage(MessageEvent event) {
     String message = event.getMessage();
     String movieName;
+
     if (message.startsWith(".rt")) {
       movieName = message.substring(".rt".length()).trim();
     } else {
       movieName = message.substring("!rt".length()).trim();
     }
+
     Map<String, String> env = System.getenv();
     String API_KEY = env.get("RT_API_KEY");
+
     if (Strings.isNullOrEmpty(API_KEY)) {
       event.getChannel().send().message("Can't RottenTomato: set the RT_API_KEY environment variable");
-    }
-    JTomato jTomato = new JTomato(API_KEY);
-    List<Movie> movies = new ArrayList<Movie>();
-    int total = jTomato.searchMovie(movieName, movies, 1);
-    if (total == 0) {
-      event.getChannel().send().message("Movie '" + movieName + "' not found. Incorrect movie name?");
-    } else {
-      Movie movie = movies.get(0);
-      event.getChannel().send().message("Name: " + movie.title + "  Audience Rating: " + movie.rating.audienceScore +
-          "%  Critics Rating: " + movie.rating.criticsScore + "%");
+    }else{
+      JTomato jTomato = new JTomato(API_KEY);
+      List<Movie> movies = new ArrayList<Movie>();
+      int total = jTomato.searchMovie(movieName, movies, 1);
+      if (total == 0) {
+          event.getChannel().send().message("Movie '" + movieName + "' not found. Incorrect movie name?");
+      } else {
+          Movie movie = movies.get(0);
+          event.getChannel().send().message("Name: " + movie.title + "  Audience Rating: " + movie.rating.audienceScore +
+                                                "%  Critics Rating: " + movie.rating.criticsScore + "%");
+      }
     }
   }
 }
