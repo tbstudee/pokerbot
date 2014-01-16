9d913a98760b9b4f82ec98d6569213d1073d795a
Rob Leidle <leidle@amazon.com>
2014-01-15 21:46:32 -0800

    Implement URL title feature.

--- src/main/java/org/poker/irc/UrlMessageEventHandler.java
+++ src/main/java/org/poker/irc/UrlMessageEventHandler.java
@@ -1,7 +1,11 @@
 package org.poker.irc;
 
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.pircbotx.hooks.events.MessageEvent;
+
+import java.io.IOException;
 
 public class UrlMessageEventHandler implements MessageEventHandler {
   @Override
@@ -11,6 +15,25 @@ public class UrlMessageEventHandler implements MessageEventHandler {
 
   @Override
   public void onMessage(MessageEvent event) {
-    event.getChannel().send().message("donk spew");
+    String message = event.getMessage();
+    String url;
+    // TODO: make this below a regex?
+    int index = message.indexOf(' ');
+    if (index <= 0) {
+      url = message;
+    } else {
+      url = message.substring(0, index - 1);
+    }
+    if (!url.startsWith("http://") && !url.startsWith("https://")) {
+      url = "http://" + url;
+    }
+    Document document;
+    try {
+      document = Jsoup.connect(url).get();
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+    String title = document.title();
+    event.getChannel().send().message(title);
   }
 }
