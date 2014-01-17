97e04dcb9a7859dfdd3bb73a62e11e1945654f5c
Tom B Studee <thomas.studee@gmail.com>
2014-01-17 17:18:58 -0600

    Merge remote-tracking branch 'origin/doge' into doge
    Conflicts:
    	src/main/java/org/poker/irc/messagehandler/DogecoinMessageEventHandler.java

--- src/main/java/org/poker/irc/messagehandler/DogecoinMessageEventHandler.java
+++ src/main/java/org/poker/irc/messagehandler/DogecoinMessageEventHandler.java
@@ -18,61 +18,77 @@ import java.text.NumberFormat;
 public class DogecoinMessageEventHandler implements MessageEventHandler {
 
     private MoneyFormatter moneyFormatter = new MoneyFormatterBuilder()
-            .appendCurrencyCode()
-            .appendAmount()
-            .toFormatter();
+          .appendCurrencyCode()
+          .appendAmount()
+          .toFormatter();
 
     @Override
     public String getDescription() {
-        return "!doge or .doge: gives you the latest info on DogeCoin";
+      return "!doge or .doge: gives you the latest info on DogeCoin";
     }
 
     @Override
     public String[] getMessagePrefixes() {
-        return new String[] { ".doge", "!doge" };
+      return new String[] { ".doge", "!doge" };
     }
 
     @Override
     public void onMessage(MessageEvent event) {
 
-        String url = "http://www.coinwarz.com/cryptocurrency/coins/dogecoin";
-        Document document;
+      String url = "http://www.coinwarz.com/cryptocurrency/coins/dogecoin";
+      Document document;
 
-        com.xeiam.xchange.dto.marketdata.Ticker btcTicker = org.poker.irc.mtGox.TickerFactory.CreateBtcTicker();
+      com.xeiam.xchange.dto.marketdata.Ticker btcTicker = org.poker.irc.mtGox.TickerFactory.CreateBtcTicker();
 
-        BigMoney thousandDogeUSD;
-        BigDecimal cryptsyPrice;
-        BigDecimal vircurexPrice;
-        BigDecimal coinexPrice;
+      BigMoney thousandDogeUSD;
+      BigDecimal cryptsyPrice;
+      BigDecimal vircurexPrice;
+      BigDecimal coinexPrice;
 
-        DecimalFormat satoshi = new DecimalFormat("0.00000000");
+      DecimalFormat satoshi = new DecimalFormat("0.00000000");
+      StringBuilder sb = new StringBuilder();
 
-        try {
-            document = Jsoup.connect(url).get();
-            cryptsyPrice = new BigDecimal(document.select("td a[href*=cryptsy] b").text());
-            vircurexPrice = new BigDecimal(document.select("td a[href*=vircurex] b").text());
-            coinexPrice = new BigDecimal(document.select("td a[href*=coinex] b").text());
+      try {
 
-            BigMoney test = btcTicker.getLast();
+        document = Jsoup.connect(url).get();
+        cryptsyPrice = new BigDecimal(document.select("td a[href*=cryptsy] b").text());
+        vircurexPrice = new BigDecimal(document.select("td a[href*=vircurex] b").text());
+        coinexPrice = new BigDecimal(document.select("td a[href*=coinex] b").text());
 
-            thousandDogeUSD = btcTicker.getLast().multipliedBy(cryptsyPrice).multipliedBy(1000);
+        thousandDogeUSD = btcTicker.getLast().multipliedBy(cryptsyPrice).multipliedBy(1000);
 
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        String[] commandParts = event.getMessage().split(" ");
+
+        BigDecimal dogeAmount;
+        if(commandParts.length == 2) {
+          dogeAmount = new BigDecimal(commandParts[1]);
+        }
+        else {
+          dogeAmount = new BigDecimal("0");
         }
 
-        StringBuilder sb = new StringBuilder();
-        sb.append("DOGE/BTC - Cryptsy: ");
-        sb.append(satoshi.format(cryptsyPrice).toString());
-        sb.append(" | Vircurex: ");
-        sb.append(satoshi.format(vircurexPrice).toString());
-        sb.append(" | CoinEx: ");
-        sb.append(satoshi.format(coinexPrice).toString());
-        sb.append(" | 1000 DOGE = ");
-        BotUtils.appendMoney(thousandDogeUSD, sb);
-        //sb.append(" | w.avg: ");
-        //sb.append(ticker.get());
-        event.getChannel().send().message(sb.toString());
-    }
+        if(dogeAmount.intValue() > 0) {
+          sb.append(dogeAmount.toString());
+          sb.append(" DOGE = ");
+          BotUtils.appendMoney(btcTicker.getLast().multipliedBy(cryptsyPrice).multipliedBy(dogeAmount), sb);
+        }
+        else {
+          sb.append("DOGE/BTC - Cryptsy: ");
+          sb.append(satoshi.format(cryptsyPrice).toString());
+          sb.append(" | Vircurex: ");
+          sb.append(satoshi.format(vircurexPrice).toString());
+          sb.append(" | CoinEx: ");
+          sb.append(satoshi.format(coinexPrice).toString());
+          sb.append(" | 1000 DOGE = ");
+          BotUtils.appendMoney(thousandDogeUSD, sb);
+        }
+
+      } catch (IOException e) {
+          throw new RuntimeException(e);
+      }
+      //sb.append(" | w.avg: ");
+      //sb.append(ticker.get());
+      event.getChannel().send().message(sb.toString());
+  }
 
 }
