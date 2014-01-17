6c0d6035a291ffb8afb7b635098a45fa0d6fd875
Tom B Studee <thomas.studee@gmail.com>
2014-01-17 16:32:17 -0600

    Merge remote-tracking branch 'origin/doge' into doge
    Conflicts:
    	src/main/java/org/poker/irc/messagehandler/DogecoinMessageEventHandler.java

--- src/main/java/org/poker/irc/BotUtils.java
+++ src/main/java/org/poker/irc/BotUtils.java
@@ -4,9 +4,6 @@ import org.joda.money.BigMoney;
 
 import java.text.NumberFormat;
 
-/**
- * Created by Tom on 1/17/14.
- */
 public class BotUtils {
     public static void appendMoney(BigMoney bigMoney, StringBuilder sb) {
         sb.append(NumberFormat.getCurrencyInstance().format(bigMoney.getAmount()));
